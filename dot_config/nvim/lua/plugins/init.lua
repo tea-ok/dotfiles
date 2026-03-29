349b17b4bc30441efe1ec082249ac437f8c210eb
tea-ok <70608286+tea-ok@users.noreply.github.com>
2026-03-30 00:36:30 +0300

    Add mini.animate to nvim

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -57,6 +57,13 @@ return {
   },
 
   {
+    "echasnovski/mini.animate",
+    version = false,
+    lazy = false,
+    opts = {},
+  },
+
+  {
     "MeanderingProgrammer/render-markdown.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
     ft = { "markdown" },
