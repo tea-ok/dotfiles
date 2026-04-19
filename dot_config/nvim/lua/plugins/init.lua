a8f841f5a6510e1d6150007cf0e90f2eb64ab066
tea-ok <70608286+tea-ok@users.noreply.github.com>
2026-04-19 17:40:28 +0300

    fix(nvim): fix snacks terminal formatting

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -85,9 +85,51 @@ return {
     "folke/snacks.nvim",
     priority = 1000,
     lazy = false,
+    init = function()
+      local group = vim.api.nvim_create_augroup("snacks_solid_floats", { clear = true })
+
+      local function apply()
+        vim.api.nvim_set_hl(0, "SnacksSolidFloat", { bg = "#1f2430" })
+        vim.api.nvim_set_hl(0, "SnacksSolidBorder", { fg = "#5c6370", bg = "#1f2430" })
+      end
+
+      apply()
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        group = group,
+        callback = apply,
+      })
+    end,
     opts = {
-      terminal = {},
+      terminal = {
+        win = { position = "float" },
+      },
       lazygit = {},
+      styles = {
+        terminal = {
+          backdrop = 60,
+          border = "rounded",
+          wo = {
+            winblend = 0,
+            winhighlight = {
+              Normal = "SnacksSolidFloat",
+              NormalNC = "SnacksSolidFloat",
+              FloatBorder = "SnacksSolidBorder",
+            },
+          },
+        },
+        lazygit = {
+          backdrop = 60,
+          border = "rounded",
+          wo = {
+            winblend = 0,
+            winhighlight = {
+              Normal = "SnacksSolidFloat",
+              NormalNC = "SnacksSolidFloat",
+              FloatBorder = "SnacksSolidBorder",
+            },
+          },
+        },
+      },
     },
   },
 
