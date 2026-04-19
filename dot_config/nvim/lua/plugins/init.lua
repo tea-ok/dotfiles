36f03f17ee232c54c1dcce9907c8e5e5b3cf38d0
tea-ok <70608286+tea-ok@users.noreply.github.com>
2026-04-19 14:41:22 +0300

    fix(nvim): tweak smear-cursor opts

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -71,6 +71,13 @@ return {
     opts = {
       hide_target_hack = true,
       cursor_color = "none",
+      stiffness = 0.8,
+      trailing_stiffness = 0.6,
+      stiffness_insert_mode = 0.7,
+      trailing_stiffness_insert_mode = 0.7,
+      damping = 0.95,
+      damping_insert_mode = 0.95,
+      distance_stop_animating = 0.5,
     },
   },
 
