cf00805e077f872ec0db1e113914761008bd1cd1
tea-ok <70608286+tea-ok@users.noreply.github.com>
2026-05-02 20:04:26 +0300

    fix(nvim): tune mini.animate scroll animation

--- nvim/.config/nvim/lua/plugins/mini-animate.lua
+++ nvim/.config/nvim/lua/plugins/mini-animate.lua
@@ -2,9 +2,23 @@ return {
   "echasnovski/mini.animate",
   version = false,
   lazy = false,
-  opts = {
-    open = { enable = false },
-    close = { enable = false },
-    cursor = { enable = false },
-  },
+  opts = function()
+    local animate = require "mini.animate"
+
+    return {
+      open = { enable = true },
+      close = { enable = true },
+      cursor = { enable = false },
+      scroll = {
+        enable = true,
+        timing = animate.gen_timing.linear({ duration = 70, unit = "total" }),
+        subscroll = animate.gen_subscroll.equal({
+          max_output_steps = 8,
+          predicate = function(total_scroll)
+            return total_scroll > 2
+          end,
+        }),
+      },
+    }
+  end,
 }
