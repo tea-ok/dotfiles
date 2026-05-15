1867f1f99e4bf22c17ae35de5893b82f6001cdd3
tea-ok <70608286+tea-ok@users.noreply.github.com>
2026-05-15 07:59:36 +0300

    feat(nvim): add rounded borders to lsp popups

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -8,13 +8,15 @@ return {
   },
 
   config = function()
+    local rounded_border = "rounded"
+
     vim.diagnostic.config({
       virtual_text = false,
       virtual_lines = { current_line = true }, -- show inline text only on the cursor line
       signs = true,
       underline = { severity = vim.diagnostic.severity.ERROR }, -- only underline errors, not warnings
       update_in_insert = false,
-      float = { border = "rounded" },
+      float = { border = rounded_border },
     })
 
     vim.api.nvim_create_autocmd('LspAttach', {
@@ -22,14 +24,18 @@ return {
       callback = function(event)
         local opts = {buffer = event.buf}
 
-        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+        vim.keymap.set('n', 'K', function()
+          vim.lsp.buf.hover({ border = rounded_border })
+        end, opts)
         vim.keymap.set('n', '<leader>k', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
         vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
         vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
         vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
         vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
         vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+        vim.keymap.set('n', 'gs', function()
+          vim.lsp.buf.signature_help({ border = rounded_border })
+        end, opts)
         vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
         vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
       end,
@@ -86,6 +92,15 @@ return {
     }
 
     cmp.setup({
+      window = {
+        completion = cmp.config.window.bordered({
+          border = rounded_border,
+        }),
+        documentation = cmp.config.window.bordered({
+          border = rounded_border,
+        }),
+      },
+
       sources = {
         {name = "nvim_lsp"},
       },
