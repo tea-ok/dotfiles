c66a4b741bff601ee404025510b3a04ddd702622
tea-ok <70608286+tea-ok@users.noreply.github.com>
2026-06-15 21:53:54 +0300

    fix(nix): fix suspend

--- system/nixos/configuration.nix
+++ system/nixos/configuration.nix
@@ -106,8 +106,11 @@
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     modesetting.enable = true;
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
+    powerManagement = {
+      enable = true;
+      kernelSuspendNotifier = true;
+      finegrained = false;
+    };
     open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
