6b475e5d26d4d8784c3fceefeac4499782c6bb0c
tea-ok <70608286+tea-ok@users.noreply.github.com>
2026-06-17 20:20:39 +0300

    feat(nvim): add dashboard

--- dotfiles/nvim/.config/nvim/lua/plugins/snacks.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,19 +1,39 @@
 return {
-  "folke/snacks.nvim",
-  priority = 1000,
-  lazy = false,
-  opts = {
-    lazygit = { enabled = true },
-    picker = {
-      enabled = true,
-    },
-    zen = {
-      toggles = { dim = true },
-      win = { width = 0.6 },
-    },
-  },
-  keys = {
-    { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
-    { "<leader>z",  function() Snacks.zen() end, desc = "Zen Mode" },
-  },
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		lazygit = { enabled = true },
+		dashboard = {
+			enabled = true,
+			sections = {
+				{ section = "header" },
+				{ section = "keys", gap = 1, padding = 1 },
+				{ section = "startup" },
+			},
+		},
+		picker = {
+			enabled = true,
+		},
+		zen = {
+			toggles = { dim = true },
+			win = { width = 0.6 },
+		},
+	},
+	keys = {
+		{
+			"<leader>gg",
+			function()
+				Snacks.lazygit()
+			end,
+			desc = "Lazygit",
+		},
+		{
+			"<leader>z",
+			function()
+				Snacks.zen()
+			end,
+			desc = "Zen Mode",
+		},
+	},
 }
